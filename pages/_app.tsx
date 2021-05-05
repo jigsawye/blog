79f4df013af8211d25f0c6049b2d9a707e1fed62
jigsawye <jigsaw.ye@gmail.com>
2021-05-05 12:36:58 +0800

    feat: send ga pageview event when route is changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,33 @@
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import Layout from '../components/Layout';
+import { GA_TRACKING_ID } from '../lib/constants';
 
-const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-);
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      (window as any).gtag('config', GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
 
 export default MyApp;
