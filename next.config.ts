05276a152af87bc5eef11199d5f172c7a87ff932
Evan Ye <jigsaw.ye@gmail.com>
2026-01-16 09:54:23 +0800

    feat: add Next.js configuration with MDX support and external package integration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX({});
 
-/** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   serverExternalPackages: ['@takumi-rs/image-response'],
